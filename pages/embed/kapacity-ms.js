d9c34395e41733d4a065544cf7e801a200e4acaf
Jan Vlcek <vlki@vlki.cz>
2022-03-09 20:31:37 +0100

    Map margin

--- pages/embed/kapacity-ms.js
+++ pages/embed/kapacity-ms.js
@@ -76,44 +76,48 @@ export default function EmbedCapacityPreschools({ baseUrl }) {
           ))}
         </div>
 
-        <EmbedMap
-          orpData={orpData}
-          krajeData={krajeData}
-          selectedOrpId={selectedOrpId}
-          setSelectedOrpId={setSelectedOrpId}
-          fillByOrpId={fillByOrpId}
-          renderTooltipContent={(orpId, feature) => (
-            <div className={styles.tooltipContent}>
-              <div className="tooltip-orp">
-                <div className="tooltip-orp-name">
-                  {feature.properties.NAZEV}
+        <div className={styles.mapWrapper}>
+          <EmbedMap
+            orpData={orpData}
+            krajeData={krajeData}
+            selectedOrpId={selectedOrpId}
+            setSelectedOrpId={setSelectedOrpId}
+            fillByOrpId={fillByOrpId}
+            renderTooltipContent={(orpId, feature) => (
+              <div className={styles.tooltipContent}>
+                <div className="tooltip-orp">
+                  <div className="tooltip-orp-name">
+                    {feature.properties.NAZEV}
+                  </div>
+                  <div className="tooltip-region">
+                    {feature.properties.VUSC_NAZEV}
+                  </div>
                 </div>
-                <div className="tooltip-region">
-                  {feature.properties.VUSC_NAZEV}
-                </div>
-              </div>
 
-              <div className="main-value-line">
-                <strong>
-                  {Math.round(orpCapacityById[orpId].ms_naplnenost_22)} %{" "}
-                  naplněnost MŠ
-                </strong>
+                <div className="main-value-line">
+                  <strong>
+                    {Math.round(orpCapacityById[orpId].ms_naplnenost_22)} %{" "}
+                    naplněnost MŠ
+                  </strong>
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].ms_kapacita.toLocaleString("cs-CZ")}{" "}
+                  celkem míst v MŠ
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].volna_mista_ms.toLocaleString(
+                    "cs-CZ"
+                  )}{" "}
+                  volných míst
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].pocet_ms.toLocaleString("cs-CZ")}{" "}
+                  mateřských škol
+                </div>
               </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].ms_kapacita.toLocaleString("cs-CZ")}{" "}
-                celkem míst v MŠ
-              </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].volna_mista_ms.toLocaleString("cs-CZ")}{" "}
-                volných míst
-              </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].pocet_ms.toLocaleString("cs-CZ")}{" "}
-                mateřských škol
-              </div>
-            </div>
-          )}
-        />
+            )}
+          />
+        </div>
 
         <div className={styles.footer}>
           <div className="footer-item">
--- pages/embed/kapacity-zs.js
+++ pages/embed/kapacity-zs.js
@@ -76,44 +76,48 @@ export default function EmbedCapacityElementarySchools({ baseUrl }) {
           ))}
         </div>
 
-        <EmbedMap
-          orpData={orpData}
-          krajeData={krajeData}
-          selectedOrpId={selectedOrpId}
-          setSelectedOrpId={setSelectedOrpId}
-          fillByOrpId={fillByOrpId}
-          renderTooltipContent={(orpId, feature) => (
-            <div className={styles.tooltipContent}>
-              <div className="tooltip-orp">
-                <div className="tooltip-orp-name">
-                  {feature.properties.NAZEV}
+        <div className={styles.mapWrapper}>
+          <EmbedMap
+            orpData={orpData}
+            krajeData={krajeData}
+            selectedOrpId={selectedOrpId}
+            setSelectedOrpId={setSelectedOrpId}
+            fillByOrpId={fillByOrpId}
+            renderTooltipContent={(orpId, feature) => (
+              <div className={styles.tooltipContent}>
+                <div className="tooltip-orp">
+                  <div className="tooltip-orp-name">
+                    {feature.properties.NAZEV}
+                  </div>
+                  <div className="tooltip-region">
+                    {feature.properties.VUSC_NAZEV}
+                  </div>
                 </div>
-                <div className="tooltip-region">
-                  {feature.properties.VUSC_NAZEV}
-                </div>
-              </div>
 
-              <div className="main-value-line">
-                <strong>
-                  {Math.round(orpCapacityById[orpId].zs_naplnenost_22)} %{" "}
-                  naplněnost ZŠ
-                </strong>
+                <div className="main-value-line">
+                  <strong>
+                    {Math.round(orpCapacityById[orpId].zs_naplnenost_22)} %{" "}
+                    naplněnost ZŠ
+                  </strong>
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].zs_kapacita.toLocaleString("cs-CZ")}{" "}
+                  celkem míst v ZŠ
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].volna_mista_zs.toLocaleString(
+                    "cs-CZ"
+                  )}{" "}
+                  volných míst
+                </div>
+                <div className="value-line">
+                  {orpCapacityById[orpId].pocet_zs.toLocaleString("cs-CZ")}{" "}
+                  základních škol
+                </div>
               </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].zs_kapacita.toLocaleString("cs-CZ")}{" "}
-                celkem míst v ZŠ
-              </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].volna_mista_zs.toLocaleString("cs-CZ")}{" "}
-                volných míst
-              </div>
-              <div className="value-line">
-                {orpCapacityById[orpId].pocet_zs.toLocaleString("cs-CZ")}{" "}
-                základních škol
-              </div>
-            </div>
-          )}
-        />
+            )}
+          />
+        </div>
 
         <div className={styles.footer}>
           <div className="footer-item">
