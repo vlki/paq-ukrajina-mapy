9dfb6a18452ecf99deb1d5b95b620ffb68bd7857
vlki <vlki@users.noreply.github.com>
2022-03-16 10:58:49 +0000

    deploy: f16aed9cb6a608709a9ea234babe20e468d9d374

---
+++ _next/static/chunks/87.76cad026097496fb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[87],{87:function(i,t,u){"use strict";u.r(t);var s=u(5893),l=(u(7294),u(4298));t.default=function(){return"vlki.github.io"===window.location.host?(0,s.jsx)(l.default,{"data-domain":"vlki.github.io",src:"https://plausible.io/js/plausible.js"}):null}},4298:function(i,t,u){i.exports=u(699)}}]);
